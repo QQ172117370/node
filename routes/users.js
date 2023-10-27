9906bdbe279398d0459fb27110dee5afaefe317d
QQ172117370 <110553649+QQ172117370@users.noreply.github.com>
2023-10-28 02:55:00 +0800

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var router = express.Router();
+const bqgFetch = require("./bqg")
+const mwFetch = require("./mw")
+const zdFetch = require("./zd")
+/* GET users listing. */
+router.get('/bqg', async function(req, res, next) {
+  img = await bqgFetch(req.query.url);
+  //res.send(img);
+  res.setHeader("Content-Type", "image/jpg");
+  res.end(img);
+});
+
+router.get('/mw', async function(req, res, next) {
+  img = await mwFetch(req.query.url);
+  //res.send(img);
+  res.setHeader("Content-Type", "image/jpg");
+  res.end(img);
+});
+
+router.get('/zd', async function(req, res, next) {
+  let json = await zdFetch(req.query.key);
+  res.send(json);
+});
+
+
+
+module.exports = router;
