dea690dac16bdfe21def11c0c44122b0d59e0a06
QQ172117370 <110553649+QQ172117370@users.noreply.github.com>
2023-10-30 11:30:34 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const bqgFetch = require("./bqg")
 const mwFetch = require("./mw")
 const zdFetch = require("./zd")
+const { f1, f2 } = require("./OCR.js")
 /* GET users listing. */
 router.get('/bqg', async function(req, res, next) {
   img = await bqgFetch(req.query.url);
@@ -24,6 +25,14 @@ router.get('/zd', async function(req, res, next) {
   res.send(json);
 });
 
-
+router.get('/imgUrl', async function (req, res, next) {
+  let aaa = await f1(req.query.url);
+  //let aaa = await f2(url)
+  let data = Buffer.from(aaa).toString('base64')
+  console.log("base64完成");
+  let aa = await f2("https://api.xhofe.top/ocr/b64/text", data)
+  console.log('识别成功');
+  res.send(aa);
+});
 
 module.exports = router;
